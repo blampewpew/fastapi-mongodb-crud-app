837f80b2ad3e86723643143356b140c2b02884e8
Bryan Lam <blam@ebsco.com>
2023-01-18 15:31:20 -0700

    Added async test for mongo db

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,25 +1,45 @@
 import os
 
+import motor.motor_asyncio
+from beanie import init_beanie
 import pytest
 from fastapi.testclient import TestClient
+from httpx import AsyncClient
 
-from app.main import app
+from app.main import get_application, start_db
 from app.core.config import Settings
+from app.models.summaries import Summary
+from app.db.database import init_db
 
 
 def get_settings_override():
-    return Settings(TESTING=1, DATABASE_URL=os.environ.get("TEST_DATABASE_URL"))
+    return Settings(TESTING=1, DATABASE_URL=os.environ.get("DATABASE_TEST_URL"))
+
+
+async def override_init_db():
+    client = motor.motor_asyncio.AsyncIOMotorClient(os.environ.get("DATABASE_URL"))
+    db = client.summaries_test
+
+    await init_beanie(database=db, document_models=[Summary])
+
+
+@pytest.fixture
+def anyio_backend():
+    return 'asyncio'
 
 
 @pytest.fixture(scope="module")
 def test_app():
-    app.dependency_overrides[Settings] = get_settings_override()
+    app = get_application()
+    # app.dependency_overrides[Settings] = get_settings_override()
     with TestClient(app) as test_client:
         yield test_client
 
 
-@pytest.fixture(scope="module")
-def test_app_with_db():
+@pytest.fixture()
+async def test_app_with_db():
+    app = get_application()
     app.dependency_overrides[Settings] = get_settings_override()
-    with TestClient(app) as test_client:
-        yield test_client
+    app.dependency_overrides[start_db] = await override_init_db()
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        yield ac
--- tests/test_summaries.py
+++ tests/test_summaries.py
@@ -3,8 +3,9 @@ import json
 import pytest
 
 
-def test_create_summary(test_app_with_db):
-    response = test_app_with_db.post("/summaries/", data=json.dumps({"url": "https://foo.bar"}))
+@pytest.mark.anyio
+async def test_create_summary(test_app_with_db):
+    response = await test_app_with_db.post("/summaries/", data=json.dumps({"url": "https://foo.bar"}))
 
     assert response.status_code == 200
     assert response.json()["url"] == "https://foo.bar"
