bb0c23f049e4b582792aad53d07e6081bca78120
Bryan Lam <blam@ebsco.com>
2023-01-18 10:35:26 -0700

    Added successful ping test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,20 +1,19 @@
 import os
 
 import pytest
-from starlette.testclient import TestClient
+from fastapi.testclient import TestClient
 
-from app.main import get_application
-from app.core.config import settings
+from app.main import app
+from app.core.config import Settings
 
 
 def get_settings_override():
-    return settings(TESTING=1, DATABASE_URL=os.environ.get("TEST_DATABASE_URL"))
+    return Settings(TESTING=1, DATABASE_URL=os.environ.get("TEST_DATABASE_URL"))
 
 
 @pytest.fixture(scope="module")
 def test_app():
-    app = get_application()
-    app.dependency_overrides[settings] = get_settings_override()
+    app.dependency_overrides[Settings] = get_settings_override()
     with TestClient(app) as test_client:
         yield test_client
 
